840c5dcafc21d820ab2b15a1280cb410811091c8
Kopo Ntheledi <kopontheledi@gmail.com>
2023-09-05 15:39:01 +0200

    adding icons, fixing naming converntion

--- next/components/events/event-item.js
+++ next/components/events/event-item.js
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import classes from'./event-item.module.css';
+import Button from "../ui/button";
 
 export default function EventItem(props) {
 const {title, image, date, location, id } = props;
@@ -16,18 +17,18 @@ const exploreLink = `/events/${id}`
 
 return <li className={classes.item}>
   <img  src={'/' + image} alt={title} />
-    <div>
-        <div>
+    <div className={classes.content}>
+        <div className={classes.summary}>
             <h2>{title}</h2>
-            <div>
+            <div className={classes.date}>
                 <time>{humanReadableDate}</time>
             </div>
-            <div>
+            <div className={classes.address}>
                 <address>{formattedAdrdress}</address>
             </div>
         </div>
-        <div>
-            <Link href={exploreLink}>Explore Event</Link>
+        <div className={classes.actions}>
+            <Button link={exploreLink}>Explore Event</Button>
         </div>
     </div>
 </li>
