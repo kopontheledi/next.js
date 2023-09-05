edb69ea01bfa3448eede5959bea6fa44c7a4c060
Kopo Ntheledi <kopontheledi@gmail.com>
2023-09-05 16:10:29 +0200

    adding zip files

--- next/pages/events/[eventId].js
+++ next/pages/events/[eventId].js
@@ -1,5 +1,9 @@
 import { useRouter } from "next/router"
 import { getEventById } from "@/dummy-data";
+import { Fragment } from "react";
+import EventSummary from "@/components/event-detail/event-detail/event-summary";
+import EventLogistics from "@/components/event-detail/event-detail/event-logistics";
+import EventContent from "@/components/event-detail/event-detail/event-content";
 
 export default function EventDetailPage(){
 const router = useRouter();
@@ -12,8 +16,17 @@ if (!event) {
 }
 
     return(
-        <div>
-            <h1>Event Detail</h1>
-        </div>
+        <Fragment>
+            <EventSummary title={event.title} />
+            <EventLogistics 
+            date={event.date}
+            address={event.location}
+            image={event.image}
+            imageAlt={event.title}
+             />
+            <EventContent>
+                <p>{event.description}</p>
+            </EventContent>
+        </Fragment>
     )
 }
