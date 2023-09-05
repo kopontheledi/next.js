ee2dc9c73b46001c50be219cf1eec17a383e5f09
Kopo Ntheledi <kopontheledi@gmail.com>
2023-09-05 15:57:11 +0200

    adding folders

--- next/components/events/event-item.js
+++ next/components/events/event-item.js
@@ -1,6 +1,9 @@
-import Link from "next/link";
 import classes from'./event-item.module.css';
 import Button from "../ui/button";
+import DateIcon from "../icons/date-icon";
+import AddressIcon from '../icons/address-icon';
+import ArrowRightIcon from '../icons/arrow-right-icon';
+
 
 export default function EventItem(props) {
 const {title, image, date, location, id } = props;
@@ -21,14 +24,20 @@ return <li className={classes.item}>
         <div className={classes.summary}>
             <h2>{title}</h2>
             <div className={classes.date}>
+                <DateIcon />
                 <time>{humanReadableDate}</time>
             </div>
             <div className={classes.address}>
+                <AddressIcon />
+                
                 <address>{formattedAdrdress}</address>
             </div>
         </div>
         <div className={classes.actions}>
-            <Button link={exploreLink}>Explore Event</Button>
+            <Button link={exploreLink}>
+                <span>Explore Event</span>
+                <span className={classes.icon}><ArrowRightIcon /></span>
+            </Button>
         </div>
     </div>
 </li>
--- next/pages/events/[eventId].js
+++ next/pages/events/[eventId].js
@@ -1,4 +1,16 @@
+import { useRouter } from "next/router"
+import { getEventById } from "@/dummy-data";
+
 export default function EventDetailPage(){
+const router = useRouter();
+
+const eventId = router.query.eventId;
+const event = getEventById(eventId)
+
+if (!event) {
+    return <p>No event found!</p>
+}
+
     return(
         <div>
             <h1>Event Detail</h1>
