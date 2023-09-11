6ee9d8c863bce5afc789d0c88b46b13445f5ebea
Kopo Ntheledi <kopontheledi@gmail.com>
2023-09-11 15:13:39 +0200

    adding async

--- next/pages/events/[eventId].js
+++ next/pages/events/[eventId].js
@@ -1,37 +1,63 @@
-import { useRouter } from "next/router"
-import { getEventById } from "@/dummy-data";
+import { getEventById, getAllEvents } from "@/helpers/api-util";
 import { Fragment } from "react";
 import EventSummary from "@/components/event-detail/event-detail/event-summary";
 import EventLogistics from "@/components/event-detail/event-detail/event-logistics";
 import EventContent from "@/components/event-detail/event-detail/event-content";
 import ErrorAlert from "@/components/ui/error-alert/error-alert";
 
-export default function EventDetailPage(){
-const router = useRouter();
+export default function EventDetailPage(props) {
+  const event = props.selectedEvent;
 
-const eventId = router.query.eventId;
-const event = getEventById(eventId)
-
-if (!event) {
+  if (!event) {
     return (
-    <ErrorAlert>
+      <ErrorAlert>
         <p>No event found!</p>
-    </ErrorAlert>
+      </ErrorAlert>
     );
+  }
+
+  return (
+    <Fragment>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </Fragment>
+  );
 }
 
-    return(
-        <Fragment>
-            <EventSummary title={event.title} />
-            <EventLogistics 
-            date={event.date}
-            address={event.location}
-            image={event.image}
-            imageAlt={event.title}
-             />
-            <EventContent>
-                <p>{event.description}</p>
-            </EventContent>
-        </Fragment>
-    )
+export async function getStaticProps(context) {
+  const eventId = context.params.eventId;
+
+  const event = await getEventById(eventId);
+
+  return {
+    props: {
+      selectedEvent: event,
+    },
+    revalidate: 30
+  };
+}
+
+//this function tells nextjs for which parameter values/ event ids it should pre-render
+
+export async function getStaticPaths() {
+  const events = await getAllEvents();
+
+  const paths = events.map((event) => ({
+    params: {
+      eventId: event.id,
+    },
+  }));
+
+  return {
+    paths: paths,
+    fallback: false
+  };
 }
